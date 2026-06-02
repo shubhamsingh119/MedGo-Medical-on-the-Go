22a966ccb04ead2856f246dde1fa9bcd34b0a045
Shubham Singh <Shubhamsingh4377@gmail.com>
2026-06-03 01:28:31 +0530

    Created API for Admin Login

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import { v2 as cloudinary } from "cloudinary"
 import doctorModel from '../models/doctorModel.js'
+import jwt from 'jsonwebtoken'
 
 
 // API for adding doctor
@@ -68,9 +69,33 @@ const addDoctor = async (req,res) => {
 
 
     } catch (error) {
-
+        console.log(error)
        res.json({success:false,message:error.message})
     }
 }
 
-export {addDoctor}
+
+
+
+// API For admin login
+const loginAdmin = async (req,res) => {
+    try {
+
+        const {email,password} = req.body
+
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+
+            const token = jwt.sign(email+password,process.env.JWT_SECRET)
+            res.json({success:true ,token})
+
+        } else{
+            res.json({success:false,message:"Invalid credentials"})
+        }
+        
+    } catch (error) {
+         console.log(error)
+       res.json({success:false,message:error.message})
+    }
+}
+
+export {addDoctor,loginAdmin}
