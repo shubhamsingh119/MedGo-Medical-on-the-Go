3fc8fbe5d130a7d3f7d009960464be3f91043171
Shubham Singh <Shubhamsingh4377@gmail.com>
2026-06-09 10:05:25 +0530

    Created Appointment Cancellation Feature for Patient

--- backend/models/appointmentModel.js
+++ backend/models/appointmentModel.js
@@ -9,7 +9,7 @@ const appointmentSchema = new mongoose.Schema({
     docData: { type: Object, required: true },
     amount: { type: Number, required: true },
     date: { type: String, required: true },
-    cancelled: { type: Boolean, dafault: false },
+    cancelled: { type: Boolean, default: false },
     payment: { type: Boolean, default: false },
     isCompleted: { type: Boolean, default: false },
 })
