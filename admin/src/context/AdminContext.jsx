9d06c3bac3fb00af92ff42e715635df87fd0a71c
Shubham Singh <Shubhamsingh4377@gmail.com>
2026-06-10 08:55:10 +0530

    Displayd and Managed Appointment for Admin panel

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -8,12 +8,13 @@ const AdminContextProvider = (props) => {
 
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
     const [doctors, setDoctors] = useState([])
+    const [appointments, setAppointments] = useState([])
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const getAllDoctors = async () => {
         try {
-            const { data } = await axios.post(backendUrl + '/api/admin/all-doctors', {}, {headers:{aToken}})
+            const { data } = await axios.post(backendUrl + '/api/admin/all-doctors', {}, { headers: { aToken } })
             if (data.success) {
                 setDoctors(data.doctors)
                 console.log(data.doctors);
@@ -29,7 +30,7 @@ const AdminContextProvider = (props) => {
     const changeAvailability = async (docId) => {
         try {
 
-            const { data } = await axios.post(backendUrl + '/api/admin/change-availability', { docId }, {headers:{aToken}})
+            const { data } = await axios.post(backendUrl + '/api/admin/change-availability', { docId }, { headers: { aToken } })
             if (data.success) {
                 toast.success(data.message)
                 getAllDoctors()
@@ -42,10 +43,48 @@ const AdminContextProvider = (props) => {
         }
     }
 
+    const getAllAppointments = async () => {
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/admin/appointments', { headers: { aToken } })
+
+            if (data.success) {
+                setAppointments(data.appointments)
+                console.log(data.appointments);
+
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const cancelAppointment = async(appointmentId) => {
+        try {
+
+            const {data} = await axios.post(backendUrl + '/api/admin/cancel-appointment', {appointmentId}, {headers:{aToken}})
+
+            if (data.success) {
+                toast.success(data.message)
+                getAllAppointments()
+            } else {
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         aToken, setAToken,
         backendUrl, doctors,
         getAllDoctors, changeAvailability,
+        appointments, setAppointments,
+        getAllAppointments,
+        cancelAppointment
     }
 
     return (
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,10 +3,11 @@ import bcrypt from 'bcrypt'
 import { v2 as cloudinary } from "cloudinary"
 import doctorModel from '../models/doctorModel.js'
 import jwt from 'jsonwebtoken'
+import appointmentModel from '../models/appointmentModel.js'
 
 
 // API for adding doctor
-const addDoctor = async (req,res) => {
+const addDoctor = async (req, res) => {
     try {
 
         const {
@@ -24,19 +25,19 @@ const addDoctor = async (req,res) => {
         const imageFile = req.file
 
         // checking for all data to add doctor
-        if(!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address){
-            return res.json({success:false,message:"Missing Details"})
+        if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address) {
+            return res.json({ success: false, message: "Missing Details" })
         }
 
         // validating email format
-        if(!validator.isEmail(email)) {
-            return res.json({success:false,message:"Please enter a valid email"})
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Please enter a valid email" })
         }
 
 
         // validating strong password
-        if(password.length<8){
-            return res.json({success:false,message:"Please enter a strong password"})
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Please enter a strong password" })
         }
 
         // hashing doctor password
@@ -44,33 +45,33 @@ const addDoctor = async (req,res) => {
         const hashedPassword = await bcrypt.hash(password, salt)
 
         // upload image to cloudinary
-        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:"image"})
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" })
         const imageUrl = imageUpload.secure_url
 
         const doctorData = {
             name,
             email,
-            image:imageUrl,
-            password:hashedPassword,
+            image: imageUrl,
+            password: hashedPassword,
             speciality,
             degree,
             experience,
             about,
             fees,
-            address:JSON.parse(address),
-            date:Date.now()
+            address: JSON.parse(address),
+            date: Date.now()
         }
 
         const newDoctor = new doctorModel(doctorData)
         await newDoctor.save()
 
-        res.json({success:true,message:"Doctor Added"})
+        res.json({ success: true, message: "Doctor Added" })
 
 
 
     } catch (error) {
         console.log(error)
-       res.json({success:false,message:error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
@@ -78,38 +79,85 @@ const addDoctor = async (req,res) => {
 
 
 // API For admin login
-const loginAdmin = async (req,res) => {
+const loginAdmin = async (req, res) => {
     try {
 
-        const {email,password} = req.body
+        const { email, password } = req.body
 
-        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
 
-            const token = jwt.sign(email+password,process.env.JWT_SECRET)
-            res.json({success:true ,token})
+            const token = jwt.sign(email + password, process.env.JWT_SECRET)
+            res.json({ success: true, token })
 
-        } else{
-            res.json({success:false,message:"Invalid credentials"})
+        } else {
+            res.json({ success: false, message: "Invalid credentials" })
         }
-        
+
     } catch (error) {
-         console.log(error)
-       res.json({success:false,message:error.message})
+        console.log(error)
+        res.json({ success: false, message: error.message })
     }
 }
 
 
 //API to get all doctors list  for admin panel
-const allDoctors = async (req,res) => {
+const allDoctors = async (req, res) => {
     try {
 
         const doctors = await doctorModel.find({}).select('-password')
-        res.json({success:true,doctors})
-        
+        res.json({ success: true, doctors })
+
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
-export {addDoctor,loginAdmin,allDoctors}
+
+// API to get all apointments list
+const appointmentsAdmin = async (req, res) => {
+    try {
+
+        const appointments = await appointmentModel.find({})
+        res.json({ success: true, appointments })
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+
+// API for appointment cancellation
+const appointmentCancel = async (req, res) => {
+    try {
+
+        const { appointmentId } = req.body
+
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+        await appointmentModel.findByIdAndUpdate(appointmentId, { cancelled: true })
+
+        // releasing doctor slot
+
+        const { docId, slotDate, slotTime } = appointmentData
+
+        const doctorData = await doctorModel.findById(docId)
+
+        let slots_booked = doctorData.slots_booked
+
+        slots_booked[slotDate] = slots_booked[slotDate].filter(e => e != slotTime)
+
+        await doctorModel.findByIdAndUpdate(docId, { slots_booked })
+
+        res.json({ success: true, message: 'Appointment Cancelled' })
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+
+
+export { addDoctor, loginAdmin, allDoctors, appointmentsAdmin, appointmentCancel }
